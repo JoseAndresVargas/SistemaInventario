cb0b56f77ab033987cf734c5905da52daf152ef4
JoseAndresVargas <andyxd76@gmail.com>
2023-10-25 20:15:08 -0600

    CRUD de Bodegas

--- SistemaInventario.Modelos/Bodega.cs
+++ SistemaInventario.Modelos/Bodega.cs
@@ -12,15 +12,15 @@ namespace SistemaInventario.Modelos
         [Key]
         public int Id { get; set; }
         
-        [Required(ErrorMessage = "Nombre es Requerido")]
-        [MaxLength(60, ErrorMessage = "Nombre debe ser Maximo 60 caracteres")]
+        [Required(ErrorMessage ="Nombre es Requerido")]
+        [MaxLength(60, ErrorMessage ="Nombre debe ser Maximo 60 caracteres")]
         public string Nombre { get; set; }
 
-        [Required(ErrorMessage = "Descripcion es Requerido")]
-        [MaxLength(100, ErrorMessage = "Descripcion debe ser Maximo 60 caracteres")]
+        [Required(ErrorMessage ="Descripcion es Requerido")]
+        [MaxLength(100, ErrorMessage ="Descripcion debe ser Maximo 60 caracteres")]
         public string Descripcion { get; set; }
 
-        [Required(ErrorMessage = "Descripcion es Requerido")]
+        [Required(ErrorMessage ="Estado es Requerido")]
         public bool Estado { get; set; }
     }
 }
